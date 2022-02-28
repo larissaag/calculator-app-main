7b519bc476fe5531c059ca3f10673dbea66f131e
Larissa <larissa-a-g@hotmail.com>
2022-02-28 16:05:05 -0300

    adicionando temas

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+const themeOne = document.getElementById('themeOne');
+const themeTwo = document.getElementById('themeTwo');
+const themeThree = document.getElementById('themeThree');
+const switchBall = document.getElementById('switchBall');
+const body = document.getElementsByTagName('body');
+
+themeOne.addEventListener('change', () => {
+    if (themeOne.checked === true) {
+        document.body.classList.remove('themeThree');
+        document.body.classList.remove('themeTwo');
+        switchBall.style.left = '4px'
+    }
+})
+themeTwo.addEventListener('change', () => {
+    if (themeTwo.checked === true) {  
+        document.body.classList.remove('themeThree');
+        document.body.classList.remove('themeOne');
+        document.body.classList.add('themeTwo');
+        switchBall.style.left = '36px';
+    }
+})
+themeThree.addEventListener('change', () => {
+    if (themeThree.checked === true) {
+        document.body.classList.remove('themeTwo');
+        document.body.classList.add('themeThree');
+        switchBall.style.left = '70px';
+    }
+})
+
 function insert (n) {
     let p = document.getElementById('result').innerHTML;
     document.getElementById('result').innerHTML = p + n;
